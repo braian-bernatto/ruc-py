c8df1a34bcf41a464f744aa16c0230ce21536974
braian-bernatto <braian5353@gmail.com>
2023-07-10 16:24:33 -0400

    input logic for numbers updated

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -55,7 +55,11 @@ const SearchInput: React.FC<Props> = ({
     }
 
     const parametro = isNumber
-      ? buscar.toString().replaceAll(',', '').replace(/\s/g, ';') //reemplaza todos los espacios en blanco por un punto y coma dejando "23424;23424"
+      ? buscar
+          .toString()
+          .replaceAll(',', ' ')
+          .replaceAll(/\s+/g, ' ')
+          .replace(/\s/g, ';') //reemplaza todos los espacios en blanco por un punto y coma dejando "23424;23424"
       : buscar.toString().replaceAll(/\s+/g, ' ').replaceAll(',', ';').trim() //quita todos los espacios extras entre palabras y con trim los espacios a los costados
 
     const getRuc = async () => {
