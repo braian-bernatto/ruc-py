2ec15ae86de1074b811a790748b87ba25117c4d3
braian-bernatto <braian5353@gmail.com>
2023-07-09 22:22:29 -0400

    button tooltip added

--- components/CopyClipboard.tsx
+++ components/CopyClipboard.tsx
@@ -15,6 +15,7 @@ const CopyClipboard: React.FC = () => {
   }
 
   const [copy, setCopy] = useState<boolean>(false)
+
   return (
     <button
       type='button'
@@ -22,7 +23,7 @@ const CopyClipboard: React.FC = () => {
         selectNode()
         setCopy(true)
       }}
-      className={`bg-white p-2 my-2 rounded-full shadow-md border-2 cursor-pointer transition-all hover:animate-none ${
+      className={`relative bg-white p-2 my-2 rounded-full shadow-md border-2 cursor-pointer transition-all hover:animate-none ${
         copy ? 'border-2 border-teal-700' : 'border-transparent animate-pulse'
       }`}
     >
@@ -42,20 +43,25 @@ const CopyClipboard: React.FC = () => {
           />
         </svg>
       ) : (
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth={1.5}
-          stroke='currentColor'
-          className='w-6 h-6 text-teal-800'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75'
-          />
-        </svg>
+        <>
+          <p className='absolute bg-white rounded-full top-[50%] translate-y-[-50%] left-[120%] px-2 shadow-xl text-teal-900 border'>
+            Copiado!
+          </p>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-6 h-6 text-teal-800'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75'
+            />
+          </svg>
+        </>
       )}
     </button>
   )
